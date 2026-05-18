b23f688c0a680e4f5c73fe5fd51262f725045308
Andrew <andrewjhoggard@gmail.com>
2026-05-17 19:08:08 -0600

    Reorganized project folders

--- Week1/lecture/scripts/index.js
+++ Week1/lecture/scripts/index.js
@@ -1,7 +1,24 @@
-let num = 55;
-if(num % 2 === 0) {
-    console.log("num is even");
+function Search(list, element) {
+    for(let item of list) {
+        if(item === element) {
+            return true;
+        }
+    }
+    return false;
 }
-else {
-    console.log("num is odd");
+function max(list) {
+    let max = 0;
+    for(let num of list) {
+        if(num > max) {
+            max = num;
+        }
+    }
+    return max;
+
+}
+const num_list = [1, 0.2, 4, 780820, 9, 200, 84, 23, 86, -463, 64, 232, 65.352, 76.32, -523, -5234131, 33131];
+console.log(max(num_list));
+let stop = 10;
+while(stop > 10) {
+    console.log(stop);
 }
